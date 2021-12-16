5806efe48ffb997470a1cc1d8f0dd56a10618974
zsxwz <wzk352202@gmail.com>
2021-12-17 07:18:44 +0800

    签到领现金，每日2毛

--- jd_cash.js
+++ jd_cash.js
@@ -1,6 +1,6 @@
 /*
 签到领现金，每日2毛～5毛
-可互助，助力码每日不变，只变日期
+已经不能可互助了
 活动入口：京东APP搜索领现金进入
 更新时间：2021-06-07
 已支持IOS双京东账号,Node.js支持N个京东账号
@@ -8,17 +8,17 @@
 ============Quantumultx===============
 [task_local]
 #签到领现金
-2 0-23/4 * * * jd_cash.js, tag=签到领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+2 5,9,16,20 * * * jd_cash.js, tag=签到领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
 ================Loon==============
 [Script]
-cron "2 0-23/4 * * *" script-path=jd_cash.js,tag=签到领现金
+cron "2 5,9,16,20 * * *" script-path=jd_cash.js,tag=签到领现金
 
 ===============Surge=================
-签到领现金 = type=cron,cronexp="2 0-23/4 * * *",wake-system=1,timeout=3600,script-path=jd_cash.js
+签到领现金 = type=cron,cronexp="2 5,9,16,20 * * *",wake-system=1,timeout=3600,script-path=jd_cash.js
 
 ============小火箭=========
-签到领现金 = type=cron,script-path=jd_cash.js, cronexpr="2 0-23/4 * * *", timeout=3600, enable=true
+签到领现金 = type=cron,script-path=jd_cash.js, cronexpr="2 5,9,16,20 * * *", timeout=3600, enable=true
  */
 const $ = new Env('签到领现金');
 const notify = $.isNode() ? require('./sendNotify') : '';
